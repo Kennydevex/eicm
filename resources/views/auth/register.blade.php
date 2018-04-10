d60b3ac7542482a7deae21676d6a5f3936ef1562
Kenny <kenny.devex@gmail.com>
2018-04-10 17:37:15 -0100

    Tools Configuration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,6 +2,17 @@
 
 @section('content')
 <div class="container">
+
+  <div class="row">
+        <div class="col s12">
+          This is an inline input field:
+          <div class="input-field inline">
+            <input id="email_inline" type="email" class="validate">
+            <label for="email_inline">Email</label>
+            <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
+          </div>
+        </div>
+      </div>
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
